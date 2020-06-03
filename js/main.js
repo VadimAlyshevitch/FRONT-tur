065768c1d2207dcc5c9f096c6e02a87eb7c157d8
Vadim <vadimpwnz228@gmail.com>
2020-06-03 20:33:41 +0300

    add styles, tubs

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// start TUBS
+
+let tab = document.querySelectorAll('.info-header-tab'),
+    info = document.querySelector('.info-header'),
+    tabContent = document.querySelectorAll('.info-tabcontent');
+const modal = document.querySelector('.modal'),
+      buttonAuth = document.querySelector('.button-auth'),
+      modalAuth = document.querySelector('.modal-auth'),
+      closeAuth = document.querySelector('.close-auth'),
+      logInForm = document.querySelector('#logInForm'),
+      loginInput = document.querySelector('#login'),
+      userName = document.querySelector('.user-name'),
+      buttonOut = document.querySelector('.button-out');
+
+function hideTabContent(a) {
+    for (let i = a; i < tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add('hide');
+    }
+}
+
+hideTabContent(1);
+
+function showTabContent(b) {
+    if (tabContent[b].classList.contains('hide')) {
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+    }
+}
+
+info.addEventListener('click', function(event) {
+    let target = event.target;
+    if (target && target.classList.contains('info-header-tab')) {
+        for(let i = 0; i < tab.length; i++) {
+            if (target == tab[i]) {
+                hideTabContent(0);
+                showTabContent(i);
+                break;
+            }
+        }
+    }
+});
+
+// start modal auth
+
+function toggleModal() {
+    modal.classList.toggle('is-open');
+}
+  
+function toggleModalAuth() {
+    modalAuth.classList.toggle('is-open');
+    loginInput.style.borderColor = '';
+    loginInput.placeholder = '';
+}
+
